08f5c3f92db3627ecce08a1c4f7e71d64e6117b3
Amir Mussa <amir.mussa99@gmail.com>
2020-07-26 16:15:25 -0500

    functional cancel button added to edit view

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -3,11 +3,19 @@ import './Edit.css';
 
 class Edit extends Component {
   // Renders the entire Edit on the DOM
+  
+  handleCancel = () => {
+    console.log('In handleCancel');
+    this.props.history.push('/details');
+  }
+
+  
   render() {
     return (
       <div className="Edit">
         <h1>Edit</h1>
         <br/>
+        <button onClick={this.handleCancel}>Cancel</button>
         
       </div>
     );
